69a739a93323305f3091d2877f36049eeac38de7
AbhinavTC <90190149+AbhinavTC@users.noreply.github.com>
2021-09-06 17:14:15 +0530

    Create listbasic.py

---
+++ listbasic.py
@@ -0,0 +1,138 @@
+#Pgm to remove the nth index character from a non-empty string
+# n = 2
+# name = 'wonder'
+# first = name[:n]
+# second = name[n+1:]
+# third = first+second
+# print(third)
+
+#pgm to replace all occurance of 'a' with '$' in a string
+# n = 'abhinav'
+# print(n.replace('a','$'))
+
+#you are given a string ,find out if the string contains:alphanumeric character,alphabetic character,digits,lower and uppercase letters
+# a = "Railway"
+# b = "545"
+# c = "ABC"
+# print(a.isdigit())
+# print(a.islower())
+# print(a.isupper())
+# print(a.isalpha())
+# print(b.isdigit())
+# print(b.islower())
+# print(c.isupper())
+
+#pgm to get a single string from two given strings, separated by a space and swap the first two letters of each string
+# a = "spider"
+# b = "super"
+# c = a[:2]
+# d = b[:2]
+# x = d+a[2:]+" "+c+b[2:]
+# print(x)
+
+#Lists
+
+# list1 = ['1','2', '3','5']
+# list1[1]=4    / replacing list item
+#list.append('7') /adding new list item
+# print(list1)
+
+#list items replace
+# list = ['apple','orange','mango']
+# list[1:2] =['watermelon','pappaya']
+# print(list)
+
+#extend list
+# list = ['bike', 'car', 'scooter']
+# list2 = ['bus','lorry', 'tractor']
+# list.extend(list2)
+# print(list)
+
+#reverse
+# list=['movie','webseries','cartoon']
+# print(list[-1])
+
+#remove list item
+# list=['india','france','italy']
+# list.remove('italy')
+# print(list)
+
+# list=['india','france','italy']
+# print(len(list))
+
+#Tuple
+# world = ('countires','peoples','places')
+# print(world)
+
+# n_tuple = ("King",[8,4,6],(1,2,3))
+# print(n_tuple)
+
+#Tuple append
+# t=('hindi','english','german')
+# y = list(t)
+# y.append('french')
+# t=y
+# print(t)
+
+#join tuple
+# w = ('k','l','m','n')
+# q = ('a','b','c','e')
+# x = w+q
+# print(x)
+
+#set
+# my = {'1','2','3'}
+# print(my)
+
+#accessing
+# l = {'7','8','9'}
+# print('11' in l)
+# print('7' in l)
+
+#add
+# list = {'hello','hi',"what's up"}
+# list.add('am')
+# print(list)
+
+#update
+# h = {'e','u','o','p'}
+# y = {'E','Q','Y'}
+# h.update(y)
+# h.remove("E") / remove a set item
+# print(h)
+
+#dictionries
+#a={"brand" :"Tata" ,"model" : "Tiago", "year": 2015 ,"year" : 2020}
+#print(a)
+#print(a["model"]) # / for accessing dict item
+#a["year"]=2019
+#print(a)
+#a["color"] = "Deytona grey"
+#a.pop("model")
+#print(a)
+
+#adding two sets
+# set1 = {'q','a','r'}
+# set2 = {'1','2','3'}
+# set3 = set1.union(set2)
+# print(set3)
+
+#x = 9
+#y = 3
+#print(x**y) /sqrt
+
+# x = 25
+# y = 3
+# print(x//y)  /floor division
+
+
+
+
+
+
+
+
+
+
+
+
